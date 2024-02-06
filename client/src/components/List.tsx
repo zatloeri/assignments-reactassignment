6acce6bd97d434b93476326e2a9888dd6846dbcc
Erik Zatloukal <zatloukal.erik@gmail.com>
2024-02-06 23:49:22 +0100

    B2: Fix layout of non-empty List component

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -5,6 +5,7 @@ const StyledList = styled.ul`
     display: flex;
     flex-grow: 1;
     flex-direction: column;
+    padding: 0.75rem 0;
 `;
 
 export const List: React.FC<PropsWithChildren> = ({ children }) => <StyledList>{children}</StyledList>;
