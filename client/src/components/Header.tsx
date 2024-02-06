9f086ed24951bd75dfb6844178e6d3571b821a16
Erik Zatloukal <zatloukal.erik@gmail.com>
2024-02-06 23:07:10 +0100

    UI1: Align header button to the right

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,25 +7,34 @@ export type HeaderProps = {
     handleAddItem: () => void;
 };
 
-const StyledDiv = styled.header`
+const StyledHeader = styled.header`
     display: flex;
+    align-items: center;
+    justify-content: space-between;
     button {
         all: unset;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
         border-radius: 50%;
         border: 1px solid;
         border-color: ${(props) => props.theme.colors.olive9};
         background-color: ${(props) => props.theme.colors.grass9};
         color: #fff;
-        width: 25px;
-        height: 25px;
+        width: 1.5625rem;
+        height: 1.5625rem;
+        svg {
+            width: 75%;
+            height: 75%;
+        }
     }
 `;
 
 export const Header: React.FC<HeaderProps> = ({ handleAddItem, children }) => (
-    <StyledDiv>
+    <StyledHeader>
         <h1>{children}</h1>
-        <button onClick={() => handleAddItem()}>
+        <button onClick={handleAddItem}>
             <PlusIcon />
         </button>
-    </StyledDiv>
+    </StyledHeader>
 );
