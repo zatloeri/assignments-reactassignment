4de9c5cf41a9a9948a9c30853494a466fd1e94b9
Erik Zatloukal <zatloukal.erik@gmail.com>
2024-02-07 00:01:36 +0100

    B1: Improve styling of Fotter component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,10 +3,16 @@ import styled from "styled-components";
 
 const StyledFooter = styled.footer`
     display: flex;
+    flex-wrap: wrap;
     margin-top: 15px;
     padding-top: 15px;
     border-top: 1px solid;
     border-color: ${(props) => props.theme.colors.olive6};
+`;
+
+const StyledFooterLine = styled.div`
+    width: 100%;
+    padding: 0.25rem 0;
 `;
 
 export type FooterProps = {
@@ -16,7 +22,7 @@ export type FooterProps = {
 
 export const Footer: React.FC<FooterProps> = ({ todoItems, doneItems }) => (
     <StyledFooter>
-        Todo: {todoItems || 0}
-        Done: {doneItems || 0}
+        <StyledFooterLine>Todo: {todoItems || 0}</StyledFooterLine>
+        <StyledFooterLine>Done: {doneItems || 0}</StyledFooterLine>
     </StyledFooter>
 );
