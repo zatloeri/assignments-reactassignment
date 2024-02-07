a34250c7516f743b9745cba38ddbf9180c48bf5d
Erik Zatloukal <zatloukal.erik@gmail.com>
2024-02-08 00:17:39 +0100

    SB2: Add story showing ListItem with visible buttons

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -11,8 +11,12 @@ const StyledListItemContainer = styled.div`
     align-items: center;
 `;
 
-const StyledActionButtons = styled.div`
-    margin-left: auto;
+const buttonForcedShowStyles = `
+    visibility: visible;
+    opacity: 1;
+`;
+
+const buttonShowOnHoverStyles = `
     opacity: 0;
     visibility: hidden;
     transition: opacity 0.3s, visibility 0.3s;
@@ -20,6 +24,12 @@ const StyledActionButtons = styled.div`
         opacity: 1;
         visibility: visible;
     }
+`;
+
+const StyledActionButtons = styled.div`
+    margin-left: auto;
+    ${({ forceHoverState }: { forceHoverState?: boolean }) =>
+        forceHoverState ? buttonForcedShowStyles : buttonShowOnHoverStyles}
 `;
 
 const StyledLabel = styled.label`
@@ -30,13 +40,20 @@ export type ListItemProps = Pick<CheckboxProps, "onCheckedChange" | "checked"> &
     label: string;
     handleEdit: () => void;
     handleRemoval: () => void;
+    forceHoverState?: boolean;
 };
 
-export const ListItem: React.FC<ListItemProps> = ({ label, handleRemoval, handleEdit, ...checkboxProps }) => (
+export const ListItem: React.FC<ListItemProps> = ({
+    label,
+    handleRemoval,
+    handleEdit,
+    forceHoverState,
+    ...checkboxProps
+}) => (
     <StyledListItemContainer>
         <Checkbox {...checkboxProps} />
         <StyledLabel>{label}</StyledLabel>
-        <StyledActionButtons>
+        <StyledActionButtons forceHoverState={forceHoverState}>
             <Button onClick={handleRemoval} colorScheme="unsafe">
                 <TrashIcon />
             </Button>
--- client/src/components/stories/ListItem.stories.tsx
+++ client/src/components/stories/ListItem.stories.tsx
@@ -6,20 +6,31 @@ const meta = {
     title: "List Item",
     component: ListItem,
     argTypes: {
+        forceHoverState: { defaultValue: false },
         handleRemoval: { action: "removed" },
         handleEdit: { action: "edited" },
     },
 } as Meta<typeof ListItem>;
 export default meta;
 type Story = StoryObj<typeof ListItem>;
-export const ToDo: Story = {
+
+export const Idle: Story = {
     args: {
         label: "Lorem ipsum dolor",
+        forceHoverState: false,
     },
 };
+
 export const Done: Story = {
     args: {
-        ...ToDo.args,
+        ...Idle.args,
         checked: true,
+    },
+};
+
+export const Hovered: Story = {
+    args: {
+        ...Idle.args,
+        forceHoverState: true,
     },
 };
